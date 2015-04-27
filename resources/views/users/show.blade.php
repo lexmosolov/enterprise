ed7fe1d3b560487d5f4bcc8f8ff5981a1d7a38e7
Lex Mosolov <lex.mosolov@gmail.com>
2015-04-28 01:51:12 +0300

    Users show readonly form - experiment with generators

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -8,21 +8,21 @@
             {!! Form::model($user) !!}
             <div class="form-group">
                 {!! Form::label('name', 'Name') !!}
-                {!! Form::text('name', null , ['class' => 'form-control', 'placeholder' => 'Name']) !!}
+                {!! Form::text('name', null , ['class' => 'form-control', 'placeholder' => 'Name', 'readonly']) !!}
             </div>
             <div class="form-group">
                 {!! Form::label('email', 'Email') !!}
-                {!! Form::text('email', null , ['class' => 'form-control', 'placeholder' => 'Name']) !!}
+                {!! Form::text('email', null , ['class' => 'form-control', 'placeholder' => 'Name', 'readonly']) !!}
             </div>
             {!! Form::close() !!}
-            {{ $user->name }}
-            {{ $user->email }}
+            {!! Form::open(array('class' => 'form-inline', 'method' => 'DELETE', 'route' => array('users.destroy', $user))) !!}
+            {!! link_to_route('users.edit', 'Edit', $user, array('class' => 'btn btn-info')) !!}
+            {!! Form::submit('Delete', array('class' => 'btn btn-danger')) !!}
+            {!! Form::close() !!}
         </div>
+
+
     </div>
 
-    {!! Form::open(array('class' => 'form-inline', 'method' => 'DELETE', 'route' => array('users.destroy', $user))) !!}
-    {!! link_to_route('users.edit', 'Edit', $user, array('class' => 'btn btn-info')) !!}
-    {!! Form::submit('Delete', array('class' => 'btn btn-danger')) !!}
-    {!! Form::close() !!}
 
 @endsection
