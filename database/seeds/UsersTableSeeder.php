c8ae9a9a4a7a23dc5eff3505f007d44eb3f037a1
Lex Mosolov <lex.mosolov@gmail.com>
2015-05-12 10:46:48 +0300

    add department_id and role_id fields to Users table seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,9 +11,9 @@ class UsersTableSeeder extends Seeder
 		DB::table('users')->delete();
 
 		$users = array(
-			['id' => 1, 'name' => 'Lex', 'email' => 'lex.mosolov@gmail.com', 'password' => '$2y$10$KDbN1J3ThQi/dUf.cKg.Fe/IHCCt/glL5vQ/sEiUw/TjFZUXZQxmy', 'remember_token' => '4XP0yApiqOu3CRpONOsGWKTv0eewU8USAtfAF6vDetA7M7gviUysHfkcaUAY', 'created_at' => new DateTime, 'updated_at' => new DateTime],
-			['id' => 2, 'name' => 'Flex', 'email' => 'flex.mosolov@gmail.com', 'password' => '$2y$10$KDbN1J3ThQi/dUf.cKg.Fe/IHCCt/glL5vQ/sEiUw/TjFZUXZQxmy', 'remember_token' => '4XP0yApiqOu3CRpONOsGWKTv0eewU8USAtfAF6vDetA7M7gviUysHfkcaUAY', 'created_at' => new DateTime, 'updated_at' => new DateTime],
-			['id' => 3, 'name' => 'Alex', 'email' => 'alex.mosolov@gmail.com', 'password' => '$2y$10$KDbN1J3ThQi/dUf.cKg.Fe/IHCCt/glL5vQ/sEiUw/TjFZUXZQxmy', 'remember_token' => '4XP0yApiqOu3CRpONOsGWKTv0eewU8USAtfAF6vDetA7M7gviUysHfkcaUAY', 'created_at' => new DateTime, 'updated_at' => new DateTime],
+			['id' => 1, 'name' => 'Lex', 'email' => 'lex.mosolov@gmail.com', 'password' => '$2y$10$KDbN1J3ThQi/dUf.cKg.Fe/IHCCt/glL5vQ/sEiUw/TjFZUXZQxmy', 'remember_token' => '4XP0yApiqOu3CRpONOsGWKTv0eewU8USAtfAF6vDetA7M7gviUysHfkcaUAY', 'department_id' => 1, 'role_id' => 1, 'created_at' => new DateTime, 'updated_at' => new DateTime],
+			['id' => 2, 'name' => 'Flex', 'email' => 'flex.mosolov@gmail.com', 'password' => '$2y$10$KDbN1J3ThQi/dUf.cKg.Fe/IHCCt/glL5vQ/sEiUw/TjFZUXZQxmy', 'remember_token' => '4XP0yApiqOu3CRpONOsGWKTv0eewU8USAtfAF6vDetA7M7gviUysHfkcaUAY', 'department_id' => 2, 'role_id' => 2, 'created_at' => new DateTime, 'updated_at' => new DateTime],
+			['id' => 3, 'name' => 'Alex', 'email' => 'alex.mosolov@gmail.com', 'password' => '$2y$10$KDbN1J3ThQi/dUf.cKg.Fe/IHCCt/glL5vQ/sEiUw/TjFZUXZQxmy', 'remember_token' => '4XP0yApiqOu3CRpONOsGWKTv0eewU8USAtfAF6vDetA7M7gviUysHfkcaUAY', 'department_id' => 3, 'role_id' => 3, 'created_at' => new DateTime, 'updated_at' => new DateTime],
 		);
 
 		DB::table('users')->insert($users);
