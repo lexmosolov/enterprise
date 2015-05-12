66419507becba34239b5ba09ff359f3bcb1d8183
Lex Mosolov <lex.mosolov@gmail.com>
2015-05-12 07:13:32 +0300

    add guarantor_id, performer_id, deadline to task seeds

--- database/seeds/TasksTableSeeder.php
+++ database/seeds/TasksTableSeeder.php
@@ -11,13 +11,13 @@ class TasksTableSeeder extends Seeder
 		DB::table('tasks')->delete();
 
 		$tasks = array(
-			['id' => 1, 'name' => 'Task 1', 'completed' => 1, 'description' => 'My first task', 'created_at' => new DateTime, 'updated_at' => new DateTime],
-			['id' => 2, 'name' => 'Task 2', 'completed' => 0, 'description' => 'My first task', 'created_at' => new DateTime, 'updated_at' => new DateTime],
-			['id' => 3, 'name' => 'Task 3', 'completed' => 0, 'description' => 'My first task', 'created_at' => new DateTime, 'updated_at' => new DateTime],
-			['id' => 4, 'name' => 'Task 4', 'completed' => 1, 'description' => 'My second task', 'created_at' => new DateTime, 'updated_at' => new DateTime],
-			['id' => 5, 'name' => 'Task 5', 'completed' => 1, 'description' => 'My third task', 'created_at' => new DateTime, 'updated_at' => new DateTime],
-			['id' => 6, 'name' => 'Task 6', 'completed' => 1, 'description' => 'My fourth task', 'created_at' => new DateTime, 'updated_at' => new DateTime],
-			['id' => 7, 'name' => 'Task 7', 'completed' => 0, 'description' => 'My fifth task', 'created_at' => new DateTime, 'updated_at' => new DateTime],
+			['id' => 1, 'name' => 'Task 1', 'completed' => 1, 'description' => 'My first task', 'created_at' => new DateTime, 'updated_at' => new DateTime, 'guarantor_id' => 1, 'performer_id' => 2, 'deadline' => new DateTime],
+			['id' => 2, 'name' => 'Task 2', 'completed' => 0, 'description' => 'My first task', 'created_at' => new DateTime, 'updated_at' => new DateTime, 'guarantor_id' => 1, 'performer_id' => 2, 'deadline' => new DateTime],
+			['id' => 3, 'name' => 'Task 3', 'completed' => 0, 'description' => 'My first task', 'created_at' => new DateTime, 'updated_at' => new DateTime, 'guarantor_id' => 1, 'performer_id' => 2, 'deadline' => new DateTime],
+			['id' => 4, 'name' => 'Task 4', 'completed' => 1, 'description' => 'My second task', 'created_at' => new DateTime, 'updated_at' => new DateTime, 'guarantor_id' => 1, 'performer_id' => 2, 'deadline' => new DateTime],
+			['id' => 5, 'name' => 'Task 5', 'completed' => 1, 'description' => 'My third task', 'created_at' => new DateTime, 'updated_at' => new DateTime, 'guarantor_id' => 1, 'performer_id' => 2, 'deadline' => new DateTime],
+			['id' => 6, 'name' => 'Task 6', 'completed' => 1, 'description' => 'My fourth task', 'created_at' => new DateTime, 'updated_at' => new DateTime, 'guarantor_id' => 1, 'performer_id' => 2, 'deadline' => new DateTime],
+			['id' => 7, 'name' => 'Task 7', 'completed' => 0, 'description' => 'My fifth task', 'created_at' => new DateTime, 'updated_at' => new DateTime, 'guarantor_id' => 1, 'performer_id' => 2, 'deadline' => new DateTime],
 		);
 
 		DB::table('tasks')->insert($tasks);
