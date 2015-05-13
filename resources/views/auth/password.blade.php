e606c2b1c532508b31eec7bd846e53979e88b05d
Lex Mosolov <lex.mosolov@gmail.com>
2015-05-13 13:21:37 +0300

    auth view reworked

--- resources/views/auth/password.blade.php
+++ resources/views/auth/password.blade.php
@@ -3,24 +3,13 @@
 @section('content')
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-12">
                 <div class="panel panel-default">
                     <div class="panel-heading">Reset Password</div>
                     <div class="panel-body">
                         @if (session('status'))
                             <div class="alert alert-success">
                                 {{ session('status') }}
-                            </div>
-                        @endif
-
-                        @if (count($errors) > 0)
-                            <div class="alert alert-danger">
-                                <strong>Whoops!</strong> There were some problems with your input.<br><br>
-                                <ul>
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
                             </div>
                         @endif
 
