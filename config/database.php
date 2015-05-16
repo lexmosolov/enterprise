17d67f7b3692357d590ebdc6f2b8fb632d307d4f
Lex Mosolov <lex.mosolov@gmail.com>
2015-05-16 17:15:46 +0300

    add cleardb

--- config/database.php
+++ config/database.php
@@ -1,5 +1,12 @@
 <?php
 
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
+
 return [
 
 	/*
@@ -54,10 +61,10 @@ return [
 
 		'mysql' => [
 			'driver' => 'mysql',
-			'host' => env('DB_HOST', 'localhost'),
-			'database' => env('DB_DATABASE', 'forge'),
-			'username' => env('DB_USERNAME', 'forge'),
-			'password' => env('DB_PASSWORD', ''),
+			'host' => env('DB_HOST', $host),
+			'database' => env('DB_DATABASE', $username),
+			'username' => env('DB_USERNAME', $password),
+			'password' => env('DB_PASSWORD', $database),
 			'charset' => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix' => '',
