757711cf9a0fe0783c33d166b5ffcc71b771177d
Lex Mosolov <lex.mosolov@gmail.com>
2015-05-21 13:15:33 +0300

    The lower-case, plural name of the class will be used as the table name unless another name is explicitly specified

--- app/User.php
+++ app/User.php
@@ -30,13 +30,6 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
 	use Authenticatable, CanResetPassword;
 
 	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
-	protected $table = 'users';
-
-	/**
 	 * The attributes that are mass assignable.
 	 *
 	 * @var array
