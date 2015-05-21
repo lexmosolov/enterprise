b0c79656a76cdc9459ed9d880fd5c399780a4dcc
Lex Mosolov <lex.mosolov@gmail.com>
2015-05-21 13:06:44 +0300

    n+1 problem solve for Users

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -20,7 +20,7 @@ class UsersController extends Controller
 	 */
 	public function index()
 	{
-		$users = User::all();
+		$users = User::with('department', 'role')->get();
 		return view('users.index', compact('users'));
 	}
 
