01130bfb2fa6f228978ec81e94b3b2b0c076f016
Lex Mosolov <lex.mosolov@gmail.com>
2015-05-23 00:01:00 +0300

    Create migration for Entry

---
+++ database/migrations/2015_05_21_153626_create_entries_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEntriesTable extends Migration
+{
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('entries', function (Blueprint $table) {
+			$table->increments('id');
+			$table->string('title');
+			$table->string('body');
+			$table->integer('creator_id')->unsigned()->index();
+			$table->foreign('creator_id')->references('id')->on('users')->onDelete('cascade');
+			$table->timestamps();
+		});
+
+		Schema::create('entry_user', function (Blueprint $table) {
+			$table->integer('entry_id')->unsigned()->index();
+			$table->foreign('entry_id')->references('id')->on('entries')->onDelete('cascade');
+			$table->integer('user_id')->unsigned()->index();
+			$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+		});
+
+		Schema::create('department_entry', function (Blueprint $table) {
+			$table->integer('entry_id')->unsigned()->index();
+			$table->foreign('entry_id')->references('id')->on('entries')->onDelete('cascade');
+			$table->integer('department_id')->unsigned()->index();
+			$table->foreign('department_id')->references('id')->on('departments')->onDelete('cascade');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('entries');
+	}
+
+}
