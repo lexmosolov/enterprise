5634f1bb45779c152644957a1adc9a1a2322547e
Lex Mosolov <lex.mosolov@gmail.com>
2015-05-23 02:02:22 +0300

    Rename creator_id to to user_id for Entry seeder

--- database/seeds/EntriesTableSeeder.php
+++ database/seeds/EntriesTableSeeder.php
@@ -11,9 +11,9 @@ class EntriesTableSeeder extends Seeder
 		DB::table('entries')->delete();
 
 		$entries = array(
-			['id' => 1, 'title' => 'Entry 1', 'body' => 'Entry 1 Body', 'creator_id' => 1, 'created_at' => new DateTime, 'updated_at' => new DateTime],
-			['id' => 2, 'title' => 'Entry 2', 'body' => 'Entry 2 Body', 'creator_id' => 1, 'created_at' => new DateTime, 'updated_at' => new DateTime],
-			['id' => 3, 'title' => 'Entry 3', 'body' => 'Entry 3 Body', 'creator_id' => 1, 'created_at' => new DateTime, 'updated_at' => new DateTime],
+			['id' => 1, 'user_id' => 1, 'title' => 'Entry 1', 'body' => 'Entry 1 Body', 'created_at' => new DateTime, 'updated_at' => new DateTime],
+			['id' => 2, 'user_id' => 1, 'title' => 'Entry 2', 'body' => 'Entry 2 Body', 'created_at' => new DateTime, 'updated_at' => new DateTime],
+			['id' => 3, 'user_id' => 1, 'title' => 'Entry 3', 'body' => 'Entry 3 Body', 'created_at' => new DateTime, 'updated_at' => new DateTime],
 		);
 
 		DB::table('entries')->insert($entries);
