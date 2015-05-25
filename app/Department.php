6f8fa075a2bc36dc19c7d5d21c4abb23d2e79b68
Lex Mosolov <lex.mosolov@gmail.com>
2015-05-25 21:09:07 +0300

    Add missed PHPDoc section

--- app/Department.php
+++ app/Department.php
@@ -9,16 +9,31 @@ class Department extends Model
 
 	protected $fillable = ['title'];
 
+	/**
+	 * Get a list of users ids associated with the given department.
+	 *
+	 * @return array
+	 */
 	public function getUserListAttribute()
 	{
 		return $this->users()->lists('id');
 	}
 
+	/**
+	 * Get the users associated by the given department.
+	 *
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+	 */
 	public function users()
 	{
 		return $this->hasMany('App\User');
 	}
 
+	/**
+	 * Get the entries associated by the given department.
+	 *
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+	 */
 	public function entries()
 	{
 		return $this->belongsToMany('App\Entry');
