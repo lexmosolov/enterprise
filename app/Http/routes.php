076f80f2f9c6d5c567ecc99a45c81a5c942e7e73
Lex Mosolov <lex.mosolov@gmail.com>
2015-05-25 23:54:34 +0300

    rename auth to owner

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,11 +18,11 @@ Route::group(['middleware' => 'auth'], function () {
 	Route::get('/', 'HomeController@index');
 
 	Route::resource('entries', 'EntriesController');
-	Route::get('/entries/{entries}/edit', ['before' => 'auth.entries', 'uses' => 'EntriesController@edit']);
+	Route::get('/entries/{entries}/edit', ['before' => 'owner.entries', 'uses' => 'EntriesController@edit']);
 	Route::model('entries', 'App\Entry');
 	// TODO: filter to middleware or maybe more useful
 	// TODO: other routes are vulnerable, example destroy
-	Route::filter('auth.entries', function ($route) {
+	Route::filter('owner.entries', function ($route) {
 		if ($route->parameter('entries')->user_id !== Auth::user()->id) {
 			return Redirect::back()->withErrors('Sorry, you can only access entries that you created');
 		}
