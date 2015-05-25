ffb1ff62f7c910cc866705471cfc3af4814dcc1d
Lex Mosolov <lex.mosolov@gmail.com>
2015-05-25 21:10:12 +0300

    Add missed PHPDoc section and relationship for entries

--- app/User.php
+++ app/User.php
@@ -25,18 +25,54 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
 	 */
 	protected $hidden = ['password', 'remember_token'];
 
+	/**
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+	 */
 	public function department()
 	{
 		return $this->belongsTo('App\Department');
 	}
 
+	/**
+	 * Get the role associated with the given user.
+	 *
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+	 */
 	public function role()
 	{
 		return $this->belongsTo('App\Role');
 	}
 
+	/**
+	 * Get the entries created by the given user.
+	 *
+	 * @return \Illuminate\Database\Eloquent\Relations\HasMany
+	 */
 	public function entries()
 	{
 		return $this->hasMany('App\Entry');
+	}
+
+	/**
+	 * Get the entries associated with the given user.
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+	 */
+	public function entriesFor()
+	{
+		return $this->belongsToMany('App\Entry');
+	}
+
+
+	/**
+	 * Get the entries created by the given user, associated with user and user department.
+	 *
+	 * @return Illuminate\Database\Eloquent\Collection
+	 */
+	public function entriesForShow()
+	{
+		// TODO: rework dat bullshit ma`fucker!
+		$entries = $this->entries;                                    // Entries created 	  by user
+		$entries = $entries->merge($this->entriesFor);                // Entries associated with user
+		return $entries->merge($this->department->entries);        // Entries associated with user department
 	}
 }
