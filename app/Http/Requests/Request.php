0f796d7b54b29cf2c61013e11d7da34ee4741c88
Lex Mosolov <lex.mosolov@gmail.com>
2015-05-27 19:30:46 +0300

    Redirect back with message for a forbidden auth operation in all requests

--- app/Http/Requests/Request.php
+++ app/Http/Requests/Request.php
@@ -1,10 +1,19 @@
 <?php namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Redirect;
 
 abstract class Request extends FormRequest
 {
 
-	//
+	/**
+	 * Redirect back with message for a forbidden auth operation.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function forbiddenResponse()
+	{
+		return Redirect::back()->with('message', 'Sorry but you have no rights to do this.');
+	}
 
 }
