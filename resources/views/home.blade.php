f0078b7c6ff8bc7a69b661d07bc4557707043b35
Lex Mosolov <lex.mosolov@gmail.com>
2015-05-28 17:11:24 +0300

    The Dashboard title now bold

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 
     <div class="panel panel-default">
         <div class="panel-heading">
-            Dashboard
+            <h3 class="panel-title">Dashboard</h3>
         </div>
 
         <div class="panel-body">
