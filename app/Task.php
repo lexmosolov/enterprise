2638754aa92d3996c650b47df847747da82994ac
Lex Mosolov <lex.mosolov@gmail.com>
2015-05-29 19:52:16 +0300

    Add PHPDoc sections to Task model

--- app/Task.php
+++ app/Task.php
@@ -7,31 +7,51 @@ class Task extends Model
 
 	protected $guarded = [];
 
+	/**
+	 * Get the guarantor associated with the given Task.
+	 *
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+	 */
 	public function guarantor()
 	{
 		return $this->belongsTo('App\User');
 	}
 
+	/**
+	 * Get the performer associated with the given Task.
+	 *
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+	 */
 	public function performer()
 	{
 		return $this->belongsTo('App\User');
 	}
 
+	/**
+	 * Get the all children associated with the given Task.
+	 *
+	 * @return \Illuminate\Database\Eloquent\Builder|static
+	 */
 	public function childrenRecursive()
 	{
 		return $this->children()->with('childrenRecursive');
 	}
 
+	/**
+	 * Get the immediate children associated with the given Task.
+	 *
+	 * @return \Illuminate\Database\Eloquent\Relations\HasMany
+	 */
 	public function children()
 	{
 		return $this->hasMany('App\Task', 'parent_id');
 	}
 
-	public function parentRecursive()
-	{
-		return $this->parent()->with('parentRecursive');
-	}
-
+	/**
+	 * Get the parent Task associated with the given Task.
+	 *
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+	 */
 	public function parent()
 	{
 		return $this->belongsTo('App\Task', 'parent_id');
