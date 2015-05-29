c26cecb97dd0d7af94c95fbcde5dff329b2d1958
Lex Mosolov <lex.mosolov@gmail.com>
2015-05-29 19:38:43 +0300

    Add parent_id to migration for hierarchical task ability

--- database/migrations/2015_05_15_221107_create_tasks_table.php
+++ database/migrations/2015_05_15_221107_create_tasks_table.php
@@ -15,15 +15,20 @@ class CreateTasksTable extends Migration
 	{
 		Schema::create('tasks', function (Blueprint $table) {
 			$table->increments('id');
-			$table->string('name')->default('');
-			$table->boolean('completed')->default(false);
-			$table->text('description')->default('');
 			$table->integer('guarantor_id')->unsigned()->index();
 			$table->foreign('guarantor_id')->references('id')->on('users')->onDelete('cascade');
 			$table->integer('performer_id')->unsigned()->index();
 			$table->foreign('performer_id')->references('id')->on('users')->onDelete('cascade');
+			$table->string('name')->default('');
+			$table->boolean('completed')->default(false);
+			$table->text('description')->default('');
 			$table->date('deadline')->nullable();
 			$table->timestamps();
+		});
+
+		Schema::table('tasks', function (Blueprint $table) {
+			$table->integer('parent_id')->unsigned()->index()->nullable();
+			$table->foreign('parent_id')->references('id')->on('tasks')->onDelete('cascade');
 		});
 	}
 
