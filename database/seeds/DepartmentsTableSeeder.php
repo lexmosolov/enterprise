e6c3baa6b2777023e2fda814e93c26ba9567d8ec
Lex Mosolov <lex.mosolov@gmail.com>
2015-05-30 09:47:41 +0300

    Add parent_id to Department seeder

--- database/seeds/DepartmentsTableSeeder.php
+++ database/seeds/DepartmentsTableSeeder.php
@@ -11,9 +11,9 @@ class DepartmentsTableSeeder extends Seeder
 		DB::table('departments')->delete();
 
 		$departments = array(
-			['id' => 1, 'title' => 'Department 1'],
-			['id' => 2, 'title' => 'Department 2'],
-			['id' => 3, 'title' => 'Department 3'],
+			['id' => 1, 'parent_id' => null, 'title' => 'Department 1'],
+			['id' => 2, 'parent_id' => 1, 'title' => 'Department 2'],
+			['id' => 3, 'parent_id' => 2, 'title' => 'Department 3'],
 		);
 
 		DB::table('departments')->insert($departments);
