5f191311076613e812cd38306afccb89b7638af4
Lex Mosolov <lex.mosolov@gmail.com>
2015-06-04 00:56:44 +0300

    Roles rework

--- app/Role.php
+++ app/Role.php
@@ -5,6 +5,11 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
 
+	/**
+	 * Indicates that the model should not be timestamped.
+	 *
+	 * @var bool
+	 */
 	public $timestamps = false;
 
 }
