4b3a71d3a7ed12dcf6673c1a424df50ee21d6469
Lex Mosolov <lex.mosolov@gmail.com>
2015-06-05 22:04:28 +0300

    Create organization migrate

---
+++ database/migrations/2015_06_05_183734_create_organizations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrganizationsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('organizations', function (Blueprint $table)
+		{
+			$table->increments('id');
+			$table->text('title');
+		});
+
+		Schema::table('departments', function (Blueprint $table)
+		{
+			$table->integer('organization_id')->unsigned()->index()->nullable();
+			$table->foreign('organization_id')->references('id')->on('organizations')->onDelete('cascade');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('organizations');
+	}
+
+}
